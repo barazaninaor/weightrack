8133f254c44f236954dc32824003a0937038652a
Silvia <caninisilvia185@gmail.com>
2026-07-25 16:13:47 +0300

    Remove babel completely to fix GitHub Actions build error

--- webpack.config.js
+++ webpack.config.js
@@ -11,17 +11,6 @@ export default {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
   devServer: {
     static: "./dist",
   },
